47da13a1383673df7b90b7369e61497222a13fa8
danielwiener <daniel@danielwiener.com>
2011-04-21 17:29:14 -0400

    layout moving divs around, adding date

--- header.php
+++ header.php
@@ -83,9 +83,9 @@ jQuery(document).ready(function($)  {
 <body>
 	<div id="supersized"></div> 
 	<div id="header">
-				<h1 id="site-title">
+				<div id="site-title">
 					<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
-				</h1>
+				</div>
 				<div id="site-description"><!-- <a href=""><?php echo $dw_category_slug; ?><a/> --><?php wp_nav_menu(); ?></div> 
 
 
