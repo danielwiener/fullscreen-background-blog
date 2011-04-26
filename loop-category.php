4fa859752709570018b8f4ed7ba10fedd799b902
danielwiener <daniel@danielwiener.com>
2011-04-26 18:34:16 -0400

    working on date and post layout and colors

--- loop-category.php
+++ loop-category.php
@@ -126,7 +126,7 @@
 <?php /* How to display all other posts. */ ?>
 
 	<?php else : ?> 
-		<div class="date"><?php echo get_the_date('M j, Y'); ?></div>
+		<div class="date"><?php echo get_the_date('m/j/y'); ?></div>
 		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			<h2 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
              
