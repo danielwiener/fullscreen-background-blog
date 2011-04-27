f4b212748fbc12a56e778ce89743ca93cbdd1c55
danielwiener <daniel@danielwiener.com>
2011-04-27 16:29:49 -0400

    layout for page.php and loop-page.php

---
+++ page.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Fullscreen Background Blog
+ * @since Fullscreen Background Blog 1.0
+ */
+
+get_header(); ?>
+
+		<div id="container">
+			<div id="content" role="main">
+				<?php $dw_page_title = $wp_query->post->post_title; ?>
+            <h1 class="page-title"><?php echo $dw_page_title; ?></h1>
+			<?php
+			/* Run the loop to output the page.
+			 * If you want to overload this in a child theme then include a file
+			 * called loop-page.php and that will be used instead.
+			 */
+			get_template_part( 'loop', 'page' );
+			?>
+
+			</div><!-- #content -->
+		</div><!-- #container -->
+
+<?php get_footer(); ?>
