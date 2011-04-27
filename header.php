fd4a8e37842c0d48a7a3f1fd3db04afddc66672b
danielwiener <daniel@danielwiener.com>
2011-04-27 12:21:34 -0400

    adding file for search form - a sidebar include, and placing it in the header

--- header.php
+++ header.php
@@ -93,8 +93,8 @@ jQuery(document).ready(function($)  {
 				<div id="site-title">
 					<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
 				</div>
-				<div id="site-description"><!-- <a href=""><?php echo $dw_category_slug; ?><a/> --><?php wp_nav_menu(); ?></div> 
-
+				<div id="site-description"><!-- <a href=""><?php echo $dw_category_slug; ?><a/> --><?php wp_nav_menu(); ?> <div id="searchform"><?php get_sidebar('searchform'); ?></div></div> 
+                
 
 			
 	   
---
+++ sidebar-searchform.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template for displaying the search form.
+ *
+ * Contains the closing of the id=main div and all content
+ * after.  Calls sidebar-footer.php for bottom widgets.
+ *
+ * @package WordPress
+ * @subpackage Fullscreen Background Blog
+ * @since Fullscreen Background Blog 1.0
+ */
+?> 
+
+<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>" >
+<div><label class="screen-reader-text" for="s"><?php __('Search for:'); ?></label>
+<input type="text" value="<?php  get_search_query(); ?>" name="s" id="s" />
+<input type="submit" id="searchsubmit" value="<?php echo esc_attr__('Search'); ?>" />
+</div>
+</form>
